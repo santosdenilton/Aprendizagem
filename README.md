27b5a1772a2f75a5c094a44cf3c1ac459e926461
santosdenilton <denilton@tuta.io>
2023-11-11 14:24:54 -0300

    finalização

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
 # Aprendizagem
 
-**Repositório pensando apenas para testar git e outros conhecimento dev**
+**Repositório pensando apenas para testar git e outros conhecimentos em dev**
+
+# Tarefas do desafio
+- [ ] Seguir Instruções Padrões **(IP)** apresentados no desafio;
+    - [X] (IP) Criar repositório no github
+        - [Aprendizagem](https://github.com/santosdenilton/Aprendizagem.git)
+    - [ ] (IP) Codar
+        - [x] (IP) Utilizar
+            - [x]  (IP) Variáveis
+                - linha 33
+            - [x]  (IP) Operadores
+                - Diversas linhas. Destaque para linhas 13-25
+            - [x]  (IP) Laços de repetição
+                - linha 33
+            - [x]  (IP) Estruturas de decisão
+                - linha 13-25
+            - [X] Constantes
+        - [ ] (IP) Criar uma variável para armazenar o nome e quantidade de experiência (XP) de um heróis
+        - [ ] (IP) Utilizar estrutura de decisão para apresentar alguma das mensagens abaixo
+            - Se XP  for menor que 1.000 = Ferro
+            - Se XP for entre 1001 e 2000 = Bronze
+            - Se XP for entre 2001 e 5000 = Prata 
+            - Se XP for entre 5001 e 7000 = Ouro
+            - Se XP for entre 7001 e 8000 = Platina
+            - Se XP for entre 8001 e 9000 = Ascendente
+            - Se XP for entre 9001 e 10000 = Imortal
+            - Se XP for acima de 10001 = Radiante
+        - [x] Repensar a estrutura original, ampliando o nível do desafio proposto
+            - [x] Criar constantes para armazenar o nome, XP, posição, line, tipo e HP de cinco heróis.
+            - [x] Incremento do bloco *function*
+                - [x] Estudar sintese *function*
+                - [x] Utilizar estrutura de *function* para verificar o nível com base do XP:
+                    - XP até 1000 = Nível Ferro
+                    - XP até 2000 = Nível Bronze
+                    - XP até 5000 = Nível Prata
+                    - XP até 7000 = Nível Outro
+                    - XP até 8000 = Nível Platina
+                    - XP até 9000 = Nível Ascendente
+                    - XP até 10000 = Nível Imortal
+                    - XP com mais de 10000 = Nível Radiante
+            - [x] Utilizar *for* para iterar e apresentar via console.log os resultados 
+    - [x] (IP) Adicionar (git add .)
+    - [x] (IP) Comitar (git commit -m"finalização")
+    - [x] (IP) Enviar link na DIO
--- index.js
+++ index.js
@@ -1,1 +1,70 @@
-código
+// Por Denilton Santos - para desafio de código do DIO
+
+// informações em arrays
+const nomesHerois = ["Jinx", "Soraka", "Katarina", "Ahri", "Akali"];
+const xpHerois = [8659, 1569, 5321, 2898, 10892];
+const posicaoHerois = ["Atiradora", "Suporte", "Jungle", "Maga", "Lutadora"];
+const lineHerois = ["Bot", "Bot", "Selva", "Mid", "Solo"];
+const tipoAtaqueHerois = ["Físico-Crítico", "Mágico", "Mágico", "Mágico", "Mágico"];
+const hpHerois = ["3802", "5056", "4128", "5679", "7987"];
+
+// Função para verificar o nível com base na XP
+function verificarNivelXP(xp) {
+  if (xp < 1000) {
+    return "Ferro";
+  } else if (xp >= 1001 && xp <= 2000) {
+    return "Bronze";
+  } else if (xp >= 2001 && xp <= 5000) {
+    return "Prata";
+  } else if (xp >= 5001 && xp <= 7000) {
+    return "Ouro";
+  } else if (xp >= 7001 && xp <= 8000) {
+    return "Platina";
+  } else if (xp >= 8001 && xp <= 9000) {
+    return "Ascendente";
+  } else if (xp >= 9001 && xp <= 10000) {
+    return "Imortal";
+  } else {
+    return "Radiante";
+  }
+}
+
+// Iteração
+for (let i = 0; i < nomesHerois.length; i++) {
+  const heroi = nomesHerois[i];
+  const xp = xpHerois[i];
+  const posicao = posicaoHerois[i];
+  const line = lineHerois[i];
+  const tipoAtaque = tipoAtaqueHerois[i];
+  const hp = hpHerois[i];
+  const nivel = verificarNivelXP(xp);
+
+// Apresentação dos resultados detalhados
+  console.log("Herói: " + heroi + " tem nível " + nivel + " com " + xp + " de experiência e " + hp + " pontos de vida")
+  console.log("   Tem a função de " + posicao)
+  console.log("   Tendo como força " + tipoAtaque)
+  console.log("   Normalmente vai na rota " + line)
+}
+
+//Resultado:
+
+//Herói: Jinx tem nível Ascendente com 8659 de experiência e 3802 pontos de vida
+//Tem a função de Atiradora
+//Tendo como força Físico-Crítico
+//Normalmente vai na rota Bot
+//Herói: Soraka tem nível Bronze com 1569 de experiência e 5056 pontos de vida  
+//Tem a função de Suporte
+//Tendo como força Mágico
+//Normalmente vai na rota Bot
+//Herói: Katarina tem nível Ouro com 5321 de experiência e 4128 pontos de vida  
+//Tem a função de Jungle
+//Tendo como força Mágico
+//Normalmente vai na rota Selva
+//Herói: Ahri tem nível Prata com 2898 de experiência e 5679 pontos de vida     
+//Tem a função de Maga
+//Tendo como força Mágico
+//Normalmente vai na rota Mid
+//Herói: Akali tem nível Radiante com 10892 de experiência e 7987 pontos de vida
+//Tem a função de Lutadora
+//Tendo como força Mágico
+//Normalmente vai na rota Solo
